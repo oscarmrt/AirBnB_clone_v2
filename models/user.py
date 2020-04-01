92a0ca79dc792064345c619b93c1b8f509dca65c
daod2003 <1253@holbertonschool.com>
2020-04-01 22:25:53 +0000

    modified file

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This is the user class"""
+import models
 from models.base_model import BaseModel
 from os import getenv
-import sqlalchemy import Column, String
+from sqlalchemy import Column, String
 
 
 class User(BaseModel):
@@ -13,7 +14,14 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
