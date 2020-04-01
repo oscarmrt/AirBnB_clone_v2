5e8f8eb0467b6396db8d1c12ac3324e3bfce3a97
oscarmrt <oscarmrt@hotmail.com>
2020-04-01 15:44:23 +0000

    modified file_storage.py working with delete function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,7 +20,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
@@ -55,3 +55,14 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj = None):
+        """Function that deletes an object
+        """
+        if obj is not None:
+            return
+        try:
+            del self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)]
+            self.save()
+        except KeyError:
+            return
