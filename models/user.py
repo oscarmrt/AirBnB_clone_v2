d0f3e557cd197a0f7e8d8e5841a22b343351d432
daod2003 <1253@holbertonschool.com>
2020-04-01 21:57:15 +0000

    update file user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy import Column, String
+from sqlalchemy.orn import relationship
 
 
 class User(BaseModel):
@@ -11,6 +14,13 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
+    if models.storage_t =='db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+
     email = ""
     password = ""
     first_name = ""
