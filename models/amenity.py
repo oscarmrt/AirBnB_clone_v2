cf6622da576098421c26fbe8243a4796eaf5d2cb
dgquintero <dgquintero02@hotmail.com>
2020-04-02 20:58:34 +0000

    amenity class added

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
