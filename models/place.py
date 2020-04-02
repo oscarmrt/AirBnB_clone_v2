3539ddd547428aca96fcbf6b4ff987b99c301a59
dgquintero <dgquintero02@hotmail.com>
2020-04-02 21:25:43 +0000

    place.py updatesetter place_amenities list added

--- models/place.py
+++ models/place.py
@@ -5,12 +5,24 @@ from models.base_model import BaseModel, Base
 from models.user import User
 from models.state import State
 from models.city import City
-from models.review import Review
+
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime, Integer, Float
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, Integer, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
+
+from os import getenv
+
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
 
 
 class Place(BaseModel, Base):
@@ -41,7 +53,9 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship("Review", backref="place")
-
+        amenities = relationship("Amenity", secondary='place_amenity',
+                                 backref='place_amenities',
+                                 viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -54,6 +68,7 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
     if models.storage_t != 'db':
         @property
         def review_method(self):
@@ -65,3 +80,14 @@ class Place(BaseModel, Base):
                 if r.place_id == self.id:
                     rlist.append(r)
             return (rlist)
+
+        @property
+        def amenity_method(self):
+            '''Getter atribute for amenity'''
+            from models.amenity import Amenity
+            alist = []
+            all_a = models.storage.all(Amenity)
+            for a in all_a.values():
+                if a.place_id == self.id:
+                    alist.append(a)
+            return (alist)
