352606a23eddc83c83d8d1271855a99f8bd354cd
oscarmrt <oscarmrt@hotmail.com>
2020-04-23 00:29:44 +0000

    file 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+from models import storage
+from models import *
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def stateList():
+    '''prints Hello HBNB!'''
+    states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardownStorage(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
